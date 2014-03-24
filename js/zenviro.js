24fc8a032644dae75d0a502870a63cd070b00329
Rob Thijssen <rthijssen@gmail.com>
2014-03-24 22:39:12 +0000

    angularjs tests

--- js/zenviro.js
+++ js/zenviro.js
@@ -1,3 +1,6 @@
-function zenviroCtrl($scope, $http) {
-  $http.get('./data/application-groups.json').then(function(response) { $scope.application-groups = response.data; });
+function appGrpsCtrl($scope, $http) {
+  $http.get('/data/application-groups.json').then(function(response) { $scope.appGrps = response.data; });
+}
+function appEnvsCtrl($scope, $http) {
+  $http.get('/data/application-groups.json').then(function(response) { $scope.appEnvs = response.data; });
 }
