4f65af4666c7792c0c5c99147a4fa86083015d8e
Rob Thijssen <rthijssen@gmail.com>
2014-03-24 21:37:36 +0000

    angularjs tests

--- plants.js
+++ plants.js
@@ -1,8 +1,8 @@
 function PlantsCtrl($scope, $http) {
   $scope.plants = [];
-  $http.get('http://plantdb.github.io/01.json').then(function(response) { $scope.plants.push(response.data); });
-  $http.get('http://plantdb.github.io/02.json').then(function(response) { $scope.plants.push(response.data); });
-  $http.get('http://plantdb.github.io/03.json').then(function(response) { $scope.plants.push(response.data); });
-  $http.get('http://plantdb.github.io/04.json').then(function(response) { $scope.plants.push(response.data); });
-  $http.get('http://plantdb.github.io/05.json').then(function(response) { $scope.plants.push(response.data); });
+  $http.get('./01.json').then(function(response) { $scope.plants.push(response.data); });
+  $http.get('./02.json').then(function(response) { $scope.plants.push(response.data); });
+  $http.get('./03.json').then(function(response) { $scope.plants.push(response.data); });
+  $http.get('./04.json').then(function(response) { $scope.plants.push(response.data); });
+  $http.get('./05.json').then(function(response) { $scope.plants.push(response.data); });
 }
